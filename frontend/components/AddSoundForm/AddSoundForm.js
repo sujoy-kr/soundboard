d0d1060bfbe4e5c624ef64698831b762d414647a
sujoy <sujoykr8@gmail.com>
2022-07-11 01:14:35 +0600

    modify .gitignore

--- frontend/components/AddSoundForm/AddSoundForm.js
+++ frontend/components/AddSoundForm/AddSoundForm.js
@@ -4,41 +4,30 @@ import Button from '../shared/Button/Button'
 import InputText from '../shared/InputText/InputText'
 
 const AddSoundForm = () => {
-    // convert mp3 file to base64
-    const convertFileToBase64 = (file) => {
-        return new Promise((resolve, reject) => {
-            const reader = new FileReader()
-            reader.readAsDataURL(file)
-            reader.onload = () => resolve(reader.result)
-            reader.onerror = (error) => reject(error)
-        })
-    }
-
-    const handleSubmit = async (e) => {
+    const handleFormSubmission = async (e) => {
         e.preventDefault()
+        const formData = new FormData(e.target)
+        const name = formData.get('name')
+        const file = formData.get('file')
 
-        if (e.target.soundName.value && e.target.soundFile.files[0]) {
-            const soundName = e.target.soundName.value
-            const soundFile = e.target.soundFile.files[0]
-            const soundFileBase64 = await convertFileToBase64(soundFile)
-
-            const data = {
-                name: soundName,
-                file: soundFileBase64,
-            }
-
-            const response = await fetch(`${process.env.API}/sound`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(data),
-            })
+        const sound = {
+            name,
+            file,
         }
+
+        const response = await fetch('/api/sound', {
+            method: 'POST',
+            body: JSON.stringify(sound),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+        const data = await response.json()
+        console.log(data)
     }
 
     return (
-        <form id={styles.AddSoundForm} onSubmit={handleSubmit}>
+        <form id={styles.AddSoundForm} onSubmit={handleFormSubmission}>
             <InputText placeholder='Sound name' type='text' name='soundName' />
             <input
                 required
